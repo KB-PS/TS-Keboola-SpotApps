4c68a904e625e167fe13d3599a05f4112697f26e
Jordan Burger <jordanrburger@gmail.com>
2022-06-29 14:49:29 -0400

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,38 @@ Keboola SpotApp Helper is a tool to help you create ThoughtSpot SpotApps.
 ''')
 
 st.sidebar.title('Welcome!')
+st.markdown('''
+### Select a SpotApp to get started.
+''')
+
+if st.button('Activity Center'):
+    st.text_input('Enter your database name:', 'KEBOOLA_1234')
+    st.text_input('Enter your schema name:', 'WORKSPACE_123456789')
+    if st.button('Replace Database Name and Schema Name'):    
+        st.write(os.system("cd output/spotapps"))
+        st.write(os.system(f"find . -type f -name '*.table.tml' -exec sed  's/KEBOOLA_5984/{db}/g' {{}} +"))
+        st.write(os.system(f"find . -type f -name '*.table.tml' -exec sed 's/WORKSPACE_10706042/{schema}/g' {{}} +"))
+        os.system("cd ~")
+        with ZipFile('Output_SpotApp.zip', 'w') as zipObj:
+       # Iterate over all the files in directory
+            for folderName, subfolders, filenames in os.walk('output'):
+                for filename in filenames:
+                    #create complete filepath of file in directory
+                    filePath = os.path.join(folderName, filename)
+                    # Add file to zip
+                    zipObj.write(filePath, basename(filePath))
+        with open("Output_SpotApp.zip", "rb") as fp:
+            btn = st.download_button(
+            label="Download ZIP",
+            data=fp,
+            file_name="myfile.zip",
+            mime="application/zip"
+        )
+
+# create a ZipFile object
+    
+        
+
 st.sidebar.markdown('''
 Please enter the database name and schema name from your Snowflake database.
 ''')
